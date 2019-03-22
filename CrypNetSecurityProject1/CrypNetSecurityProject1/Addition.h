87ea05eea80e9f836bf953de72076bd3dbd83814
nam.nguyen-xuan <nam.nguyen-xuan@banvien.com.vn>
2019-03-22 17:20:03 +0700

    Add Steganography algorithrm

--- CrypNetSecurityProject1/CrypNetSecurityProject1/Addition.h
+++ CrypNetSecurityProject1/CrypNetSecurityProject1/Addition.h
@@ -1,3 +1,7 @@
 //TODO: Write functions which has not been included in Labs to encrypt or decrypt the input file.
+#include <opencv/highgui.h>
 
-void additon_function(char* inputfile, char*outputfile);
+using namespace cv;
+
+void steganography_encode(char* inputfile, Mat image, char*outputfile);
+void steganography_decode(Mat image, char*outputfile);
--- CrypNetSecurityProject1/CrypNetSecurityProject1/Main.cpp
+++ CrypNetSecurityProject1/CrypNetSecurityProject1/Main.cpp
@@ -4,15 +4,46 @@
 #include "Symmetric.h"
 
 
+#define SYMMETRIC_ENCODE	10
+#define SYMMETRIC_DECODE	11
+#define ASYMMETRIC_ENCODE	20
+#define ASYMMETRIC_DECODE	21
+#define STREGAN_ENCODE		30
+#define STREGAN_DECODE		31
+
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	/* //TODO:
-		Get parammeter to call others function in header files	
-	*/
-
-	cout << "Hello, World!\n";
+	int tasks = 0;
+	if (argc > 1) {
+		sscanf(argv[1], "%d", &tasks);
+	}
+	else
+	{
+		return 0;
+	}
+	switch (tasks)
+	{
+	case 30:
+	{
+		Mat image = imread(argv[3]);
+		steganography_encode(argv[2], image, argv[4]);
+		break;
+	}
+	case 31:
+	{
+		Mat cipherImage = imread(argv[2]);
+		steganography_decode(cipherImage, argv[3]);
+	}
+	default:
+		return 0;
+	}
+	//char image_file[] = "../input_files/dennis_ritchie.png";
+	//char text[] = "../input_files/text.txt";
+	//char outImage[] = "../output_files/output_image.png";
+	//char plaintText[] = "../output_files/plainttext.txt";
 	system("pause");
 	return 0;
 }

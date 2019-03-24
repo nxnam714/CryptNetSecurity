4a9b1eddb0fe370e52b39a722a7fd18ddabe50c0
nam.nguyen-xuan <nam.nguyen-xuan@banvien.com.vn>
2019-03-24 13:35:03 +0700

    [Nam] Add RSA

--- CrypNetSecurityProject1/CrypNetSecurityProject1/Asymmetric.h
+++ CrypNetSecurityProject1/CrypNetSecurityProject1/Asymmetric.h
@@ -1,3 +1,39 @@
-//TODO: Write asymmetric functions to encrypt or decrypt the input file.
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
 
-void asymmetric_function(char* inputfile, char*outputfile);
+#ifndef RSA_ALGORITHM_H
+#define RSA_ALGORITHM_H
+
+#define KEY_LENGTH       2048
+#define PUBLIC_EXPONENT  59     //Public exponent should be a prime number.
+#define PUBLIC_KEY_PEM   1
+#define PRIVATE_KEY_PEM  0
+
+#define LOG(x)               \
+        cout << x << endl;   \
+
+ /*
+  * @brief   create_RSA function creates public key and private key file
+  *
+  */
+RSA * create_RSA(RSA *keypair, int pem_type, char *file_name);
+
+/*
+ * @brief   public_ecrypt function encrypts data.
+ * @return  If It is fail, return -1
+ */
+int public_encrypt(int flen, unsigned char* from, unsigned char *to, RSA* key, int padding);
+
+/*
+ * @brief   private_decrypt function decrypt data.
+ * @return  If It is fail, return -1
+ */
+int private_decrypt(int flen, unsigned char* from, unsigned char *to, RSA* key, int padding);
+
+/*
+ * @brief   create_ecrypted_file function creates .bin file. It contains encrypted data.
+ */
+void create_encrypted_file(char* encrypted, RSA * key_pair);
+
+#endif //RSA_ALGORITHM_H
--- CrypNetSecurityProject1/CrypNetSecurityProject1/Main.cpp
+++ CrypNetSecurityProject1/CrypNetSecurityProject1/Main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <fstream>
+
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
 #include "Addition.h"
 #include "Asymmetric.h"
 #include "Symmetric.h"
+#include "Main.h"
 
 
 #define SYMMETRIC_ENCODE	10
 #define SYMMETRIC_DECODE	11
-#define ASYMMETRIC_ENCODE	20
-#define ASYMMETRIC_DECODE	21
+#define RSA_CREATE			20
+#define RSA_ENCODE			21
+#define RSA_DECODE			22
 #define STREGAN_ENCODE		30
 #define STREGAN_DECODE		31
 
 
 using namespace std;
+
 
 int main(int argc, char *argv[])
 {
@@ -22,28 +31,109 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		system("pause");
 		return 0;
 	}
 	switch (tasks)
 	{
-	case 30:
+	case STREGAN_ENCODE:
 	{
 		Mat image = imread(argv[3]);
 		steganography_encode(argv[2], image, argv[4]);
 		break;
 	}
-	case 31:
+	case STREGAN_DECODE:
 	{
 		Mat cipherImage = imread(argv[2]);
 		steganography_decode(cipherImage, argv[3]);
 	}
-	default:
-		return 0;
+	case RSA_CREATE:
+	{
+		RSA *keypair = RSA_generate_key(KEY_LENGTH, PUBLIC_EXPONENT, NULL, NULL);
+
+		create_RSA(keypair, PRIVATE_KEY_PEM, argv[2]);
+		create_RSA(keypair, PUBLIC_KEY_PEM, argv[3]);
+
+		RSA_free(keypair);
+		break;
 	}
-	//char image_file[] = "../input_files/dennis_ritchie.png";
-	//char text[] = "../input_files/text.txt";
-	//char outImage[] = "../output_files/output_image.png";
-	//char plaintText[] = "../output_files/plainttext.txt";
+	case RSA_ENCODE:
+	{
+		RSA *private_key = NULL;
+		RSA *public_key = NULL;
+		FILE  *fp = NULL;
+
+		char message[KEY_LENGTH / 8];
+		char *encrypt = NULL;
+		char *decrypt = NULL;
+
+		fp = fopen(argv[2], "r");
+		fgets(message, KEY_LENGTH / 8, fp);
+		fclose(fp);
+
+		fp = fopen(argv[3], "rb");
+		PEM_read_RSAPrivateKey(fp, &private_key, NULL, NULL);
+		fclose(fp);
+
+		fp = fopen(argv[4], "rb");
+		PEM_read_RSAPublicKey(fp, &public_key, NULL, NULL);
+		fclose(fp);
+
+		encrypt = (char*)malloc(RSA_size(public_key));
+		int encrypt_length = public_encrypt(strlen(message) + 1, (unsigned char*)message, (unsigned char*)encrypt, public_key, RSA_PKCS1_OAEP_PADDING);
+		if (encrypt_length == -1) {
+			LOG("An error occurred in public_encrypt() method");
+		}
+		LOG("Data has been encrypted.");
+
+		create_encrypted_file(encrypt, public_key);
+		LOG("Encrypted file has been created.");
+
+		RSA_free(private_key);
+		RSA_free(public_key);
+		free(encrypt);
+		break;
+	}
+	case RSA_DECODE:
+	{
+		RSA *private_key = NULL;
+		FILE  *fp = NULL;
+		char *encrypt = NULL;
+		char *decrypt = NULL;
+
+		fp = fopen(argv[2], "rb");
+		PEM_read_RSAPrivateKey(fp, &private_key, NULL, NULL);
+		fclose(fp);
+
+		ifstream file(argv[3], ios::in | ios::binary | ios::ate);
+		int encrypt_length = file.tellg();
+		file.seekg(0, ios::beg);
+
+		encrypt = (char*)malloc(RSA_size(private_key));
+		file.read(encrypt, encrypt_length);
+
+		decrypt = (char *)malloc(encrypt_length);
+		int decrypt_length = private_decrypt(encrypt_length, (unsigned char*)encrypt, (unsigned char*)decrypt, private_key, RSA_PKCS1_OAEP_PADDING);
+		if (decrypt_length == -1) {
+			LOG("An error occurred in private_decrypt() method");
+		}
+		LOG("Data has been decrypted.");
+
+		FILE *decrypted_file = fopen(argv[4], "w");
+		fwrite(decrypt, sizeof(*decrypt), decrypt_length - 1, decrypted_file);
+		fclose(decrypted_file);
+		LOG("Decrypted file has been created.");
+
+		RSA_free(private_key);
+		free(encrypt);
+		free(decrypt);
+		LOG("OpenSSL_RSA has been finished.");
+		break;
+	}
+	default:
+		break;
+	}
+	cout << "Hello World\n";
 	system("pause");
 	return 0;
 }
